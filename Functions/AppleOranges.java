126a81e380b2a80771961711ba04c48d89ed5617
Pratik Kumar <pr2tik1@gmail.com>
2022-10-10 21:11:45 +0530

    Added Functions

---
+++ Functions/AppleOranges.java
@@ -0,0 +1,8 @@
+package Functions;
+
+public class AppleOranges {
+    static int LikesBoth(int N, int A, int B){
+        int both = A + B - N;
+        return both;
+    }
+}
---
+++ Functions/MohitAvg.java
@@ -0,0 +1,8 @@
+package Functions;
+
+public class MohitAvg {
+    static int Average(int A,int B, int C){
+        int avg =(A+B+C)/3;
+        return avg;
+    }
+}
